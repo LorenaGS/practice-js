82cfbe6b36d08981e0f9527a027337b06175cee2
Deici Lorena Gomez Suarez <dlgomezs@unal.edu.co>
2022-10-21 09:44:04 -0500

    Add shopping cart list

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const menuOrders = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
 const hamMenu = document.querySelector('#hamMenu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const productsCart = document.querySelector('#listProductsCart');
+
 
 menuOrders.addEventListener('click', toggleDesktopMenu);
 hamMenu.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    productsCart.classList.add('inactive');
 }
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+    productsCart.classList.add('inactive');
+}
+
+function toggleShoppingCart(){
+    productsCart.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 }
